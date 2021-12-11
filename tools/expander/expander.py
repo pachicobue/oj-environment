b8d667be89bc086e06595dfc28bce736b21a418a
Pachicobue <tigerssho@gmail.com>
2021-12-11 22:52:58 +0900

    expanderがコメントを残すように修正

--- tools/expander/expander.py
+++ tools/expander/expander.py
@@ -10,18 +10,19 @@ _logger = getLogger(__name__)  # type:Logger
 
 
 class Expander:
-    def __init__(self,
-                 source_filename: str,
-                 include_dir_names: List[str],
-                 definitions: List[str]):
+    def __init__(
+        self, source_filename: str, include_dir_names: List[str], definitions: List[str]
+    ):
         self.source_file_path = Path(source_filename).resolve()  # type:Path
         self.output_file_path = self.source_file_path.with_name(
-            self.source_file_path.stem + '_submission' + self.source_file_path.suffix)
+            self.source_file_path.stem + "_submission" + self.source_file_path.suffix
+        )
         self.tmp_file_path = self.source_file_path.with_name(
-            self.source_file_path.stem + '_tmp' + self.source_file_path.suffix)
-        _logger.debug('source_file_path: {}'.format(self.source_file_path))
-        _logger.debug('output_file_path: {}'.format(self.output_file_path))
-        _logger.debug('tmp_file_path: {}'.format(self.tmp_file_path))
+            self.source_file_path.stem + "_tmp" + self.source_file_path.suffix
+        )
+        _logger.debug("source_file_path: {}".format(self.source_file_path))
+        _logger.debug("output_file_path: {}".format(self.output_file_path))
+        _logger.debug("tmp_file_path: {}".format(self.tmp_file_path))
 
         self.include_directories = []  # type:List[Path]
         for dir_name in include_dir_names:
@@ -31,37 +32,44 @@ class Expander:
         self.definitions = definitions  # type:List[str]
 
     def expand_include(self) -> None:
-        commands = [compiler_for_expander, str(self.source_file_path),
-                    '-o', str(self.tmp_file_path),
-                    '-E', '-P']
+        commands = [
+            compiler_for_expander,
+            str(self.source_file_path),
+            "-o",
+            str(self.tmp_file_path),
+            "-nostdinc",
+            "-E",
+            "-P",
+            "-C",
+        ]
         for macro in self.definitions:
-            commands.append('-D')
+            commands.append("-D")
             commands.append(macro)
         for directory in self.include_directories:
-            commands.append('-I')
+            commands.append("-I")
             commands.append(str(directory))
         subprocess.run(commands, cwd=self.source_file_path.parent)
 
     def post_process(self, save_output: bool, save_to_clipboard: bool) -> None:
         output = self._beautify_output(open(str(self.tmp_file_path)).read())
-        _logger.debug('output = \n{}'.format(output))
+        _logger.debug("output = \n{}".format(output))
         self.tmp_file_path.unlink()
         if save_output:
-            with open(str(self.output_file_path), mode='w') as f:
+            with open(str(self.output_file_path), mode="w") as f:
                 f.write(output)
         if save_to_clipboard:
             pyperclip.copy(output)
 
     def _beautify_output(self, source: str) -> str:
         headers = set()
-        include_pattern = re.compile('#pragma INCLUDE <(.*)>')
-        output = ''
+        include_pattern = re.compile("#pragma INCLUDE <(.*)>")
+        output = ""
         for line in source.splitlines():
-            if re.match('^\s*$', line):
+            if re.match("^\s*$", line):
                 continue
             match = include_pattern.match(line)
             if match:
-                headers.add('#include <{}>'.format(match[1]))
+                headers.add("#include <{}>".format(match[1]))
             else:
-                output += line + '\n'
-        return '\n'.join(headers) + '\n' + output
+                output += line + "\n"
+        return "\n".join(headers) + "\n" + output
